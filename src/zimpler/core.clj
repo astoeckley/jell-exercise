9c008db8f3a3bf285e21bf5396509238fc5b32ec
Andrew Stoeckley <andrewjstoeckley@gmail.com>
2018-02-25 21:01:08 +0100

    Initial commit with main function for receiving the file name containing the exercise commands.

---
+++ src/zimpler/core.clj
@@ -0,0 +1,8 @@
+(ns zimpler.core)
+
+
+(defn -main
+  "Entry point for running program from command line. Pass a file name which contains the exercise commands."
+  [file]
+  (println "Received file name:" file))
+
---
+++ test/zimpler/core_test.clj
@@ -0,0 +1,7 @@
+(ns zimpler.core-test
+  (:require [clojure.test :refer :all]
+            [zimpler.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
